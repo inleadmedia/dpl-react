2e47730e7fe751d35cf92e645e3809f0ea6ef07a
Anton Vashurkin <cronosfera2@gmail.com>
2025-11-21 11:03:30 +0300

    Inlead release 2025.46.0+inlead.21.11.2025

--- src/core/dbc-gateway/generated/graphql.ts
+++ src/core/dbc-gateway/generated/graphql.ts
@@ -940,6 +940,8 @@ export type Manifestation = {
    * @deprecated Use 'Manifestation.contents' instead expires: 01/11-2025
    */
   manifestationParts?: Maybe<ManifestationParts>;
+  /** Field for presenting bibliographic records in MARC format */
+  marc?: Maybe<MarcRecord>;
   /** The type of material of the manifestation based on bibliotek.dk types */
   materialTypes: Array<MaterialType>;
   /** Notes about the manifestation */
@@ -1078,6 +1080,22 @@ export type Manifestations = {
    * Only one manifestation per unit is returned.
    */
   searchHits?: Maybe<Array<SearchHit>>;
+};
+
+export type MarcRecord = {
+  __typename?: "MarcRecord";
+  /** The library agency */
+  agencyId: Scalars["String"]["output"];
+  /** The bibliographic record identifier */
+  bibliographicRecordId: Scalars["String"]["output"];
+  /** The MARC record collection content as marcXchange XML string */
+  content: Scalars["String"]["output"];
+  /** The serialization format of the MARC record content. Defaults to 'marcXchange' */
+  contentSerializationFormat: Scalars["String"]["output"];
+  /** Flag indicating whether or not the record is deleted */
+  deleted: Scalars["Boolean"]["output"];
+  /** The marc record identifier */
+  id: Scalars["String"]["output"];
 };
 
 export type MaterialType = {
@@ -2185,6 +2203,8 @@ export type Work = {
   mainLanguages: Array<Language>;
   /** Details about the manifestations of this work */
   manifestations: Manifestations;
+  /** Field for presenting bibliographic records in MARC format */
+  marc?: Maybe<MarcRecord>;
   /** The type of material of the manifestation based on bibliotek.dk types */
   materialTypes: Array<MaterialType>;
   /** Relations to other manifestations */
