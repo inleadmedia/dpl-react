de6f8f8044eaf1f28c3da062efc7da0e93699f3a
Kasper Garnæs <kasperg@users.noreply.github.com>
2025-12-03 15:34:29 +0100

    Stop redirects to materials from suggestions with highlighted material
    Suggestions from FBI have an odd data structure. They may relate to
    topics or authors but still include a reference to a work. At the same
    time we want to extract and display selected works as a part of the
    suggestions.
    
    In the current form these two types can get mixed up if a suggestion
    is for an author or topic but include a reference to a work which is
    also displayed directly. This will lead to the user being sent
    directly to the work even though she selected the author/topic.
    
    Avoid this by comparing the selected suggestion with the material
    suggestions.
    
    If the suggestion is a material suggestion when we can proceed as now.
    Otherwise use the default approach by directing to the search page.
    
    The helper isDisplayedAsWorkSuggestion got so simple and is only used
    once so we might as well inline it.
    
    DDFSAL-474

--- src/apps/search-header/search-header.tsx
+++ src/apps/search-header/search-header.tsx
@@ -23,8 +23,7 @@ import {
   determineSuggestionTerm,
   findNonWorkSuggestion,
   getAutosuggestCategoryList,
-  getInitialSearchQuery,
-  isDisplayedAsWorkSuggestion
+  getInitialSearchQuery
 } from "./helpers";
 import { useEventStatistics } from "../../core/statistics/useStatistics";
 import { statistics } from "../../core/statistics/statistics";
@@ -221,10 +220,7 @@ const SearchHeader: React.FC = () => {
       return;
     }
     // If this item is shown as one of work suggestions redirect to material page.
-    if (
-      selectedItem.work?.workId &&
-      isDisplayedAsWorkSuggestion(selectedItem.work, materialData)
-    ) {
+    if (selectedItem.work?.workId && materialData.includes(selectedItem)) {
       track("click", {
         id: statistics.autosuggestClick.id,
         name: statistics.autosuggestClick.name,
