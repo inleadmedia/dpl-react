64462a900239250de73605663644311d28108657
Thomas Fini Hansen <xen@xen.dk>
2026-01-22 09:25:25 +0100

    Only add branch filter when branch list isn't empty
    Not providing the branch filter and giving it the empty list is not
    equivalent. The former searches disregarding branch (which still
    limits to the library's total inventory) while the latter limits to
    materials without a branch (which is primarily electronic materials).

--- src/components/facet-browser/helper.ts
+++ src/components/facet-browser/helper.ts
@@ -61,7 +61,7 @@ export const createFilters = (
 ) => {
   return {
     ...formatFacetTerms(facets),
-    ...(branchIdList ? { branchId: branchIdList } : {})
+    ...(branchIdList.length > 0 ? { branchId: branchIdList } : {})
   };
 };
 
