558ff1e7f23d7d2901da3087997fe656d577cbd0
Anton Vashurkin <cronosfera2@gmail.com>
2026-02-02 11:11:47 +0300

    EONEXT-207 - Added expanded opening hours libraries at the sidebar by default.

--- src/core/storybook/serviceUrlArgs.ts
+++ src/core/storybook/serviceUrlArgs.ts
@@ -263,4 +263,6 @@ if (typeof window === "object" && process.env.USE_DEVELOPMENT_OPTIONS === "true"
 
   const agencyConfig = { id: "911130" };
   document.body.setAttribute("data-agency-config", JSON.stringify(agencyConfig));
+
+  document.body.setAttribute("data-opening-hours-sidebar-expanded", "10, 12, 24");
 }
