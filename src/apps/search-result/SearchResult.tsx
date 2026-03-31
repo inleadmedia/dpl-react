7fed2a6a5db4ec277be7d300d52773594c5f7d57
Dinu Rodnitchi <rodnitchi@gmail.com>
2026-03-31 17:31:01 +0300

    EONEXT-214: Add branches filter on search.

--- src/apps/search-result/SearchResult.tsx
+++ src/apps/search-result/SearchResult.tsx
@@ -13,7 +13,7 @@ import {
 import { Work } from "../../core/utils/types/entities";
 import { getCurrentLocation, redirectTo } from "../../core/utils/helpers/url";
 import { useText } from "../../core/utils/text";
-import { cleanBranchesId, TBranch } from "../../core/utils/branches";
+import useGetSearchBranches from "../../core/utils/branches";
 import SearchResultInvalidSearch from "./search-result-not-valid-search";
 import { useUrls } from "../../core/utils/url";
 import { useConfig } from "../../core/utils/config";
@@ -40,10 +40,7 @@ const SearchResult: React.FC<SearchResultProps> = ({ q, pageSize }) => {
   const [hitcount, setHitCount] = useState<number>(0);
   const minimalQueryLength = 1;
   const config = useConfig();
-  const branches = config<TBranch[]>("branchesConfig", {
-    transformer: "jsonParse"
-  });
-  const cleanBranches = cleanBranchesId(branches);
+  const cleanBranches = useGetSearchBranches();
 
   const { openDialogWithContent, closeDialog, dialogRef } = useDialog();
 
